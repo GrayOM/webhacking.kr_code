44fb91526e232474c3e47480807c93e62e54611c
GrayOM <tmdals720@naver.com>
2024-03-28 17:16:37 +0900

    설명(주석)추가

--- webhacking.kr_old33-2.py
+++ webhacking.kr_old33-2.py
@@ -2,7 +2,7 @@ import requests
 
 url="https://webhacking.kr/challenge/bonus-6/lv2.php"
 
-post_1_2 = {"post" : "hehe" , "post2" : "hehe2"}
+post_1_2 = {"post" : "hehe" , "post2" : "hehe2"} #post,post2 : 를통해 name : value 로 될수있음
 
 re_post = requests.post(url,data=post_1_2) #data -> Post대한 매개변수
 

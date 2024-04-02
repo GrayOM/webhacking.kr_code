d480122758f081de56b6f4a35439e8e9cbb17845
GrayOM <tmdals720@naver.com>
2024-04-02 15:33:26 +0900

    주석업데이트

--- webhacking.kr_old32.py
+++ webhacking.kr_old32.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'https://webhacking.kr/challenge/code-5/?hit=자신의 webhacking.kr 아이디입력'
 
-cookie = {'PHPSESSID':'자신의 PHPSESSID 입력','vote_check' :''}
+cookie = {'PHPSESSID':'자신의 PHPSESSID 입력','vote_check' :''} #vote_check 는 공백으로 해도됩니다
 
 for i in range(100):
     re_vote = requests.get(url, cookies=cookie)
